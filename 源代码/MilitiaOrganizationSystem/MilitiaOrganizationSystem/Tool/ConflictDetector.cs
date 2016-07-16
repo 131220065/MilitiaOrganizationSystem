3fcf3c48c48b3f6aef70fe97ea368fca87abc1b1
131220065 <1519261432@qq.com>
2016-07-16 23:51:59 +0800

    将字典树的结构由Node改成了object的形式，极大地减少了字典树占用的内存

--- 源代码/MilitiaOrganizationSystem/MilitiaOrganizationSystem/Tool/ConflictDetector.cs
+++ 源代码/MilitiaOrganizationSystem/MilitiaOrganizationSystem/Tool/ConflictDetector.cs
@@ -8,35 +8,15 @@ namespace MilitiaOrganizationSystem
 {
     public class ConflictDetector
     {//用于检测身份证号冲突，长度都一致才不会有bug
-        private Node root;
         public Dictionary<string, List<string>> conflictDict { get; set; }
+        //冲突字典
+        private object root;//根节点
+
         public ConflictDetector()
-        {
-            root = new Node();
+        {//构造函数
             conflictDict = new Dictionary<string, List<string>>();
+            root = new object[11];//根节点是长度为11的数组
         }
-        private class Node
-        {
-            public Node[] next { get; set; }
-            public Node()
-            {
-                next = new Node[11];//身份证号由11种字符组成，0~9和X
-                for(int i = 0; i < 11; i++)
-                {
-                    next[i] = null;
-                }
-            }
-        }
-
-        private class TagNode : Node
-        {
-            public string databaseName { get; set; }
-            public TagNode(string database)
-            {//数据库名
-                databaseName = database;
-            }
-        }
-
 
         private char parse(char ch)
         {
@@ -44,7 +24,7 @@ namespace MilitiaOrganizationSystem
             return ch > '9' || ch < '0' ? (char)10 : (char)(ch - '0');
         }
         public void insertAndDetectConflicts(string credentialNumber, string database)
-        {
+        {//插入并检测冲突
             string conflictDatabase;
             if (!insert(credentialNumber, database, out conflictDatabase))
             {//没有插入成功，表示有冲突
@@ -64,28 +44,28 @@ namespace MilitiaOrganizationSystem
 
         private bool insert(string credinumber, string currentDatabase, out string conflictDatabase)
         {//插入身份证号，如果原来已经存在，返回原来的conflictDatabase,这个存储于最后一个结点的字典中
-            Node[] nodeDict = root.next;
+            object[] nodeDict = (object[])root;
             bool isExist = true;
             char dictIndex;
             for (int i = 0; i < credinumber.Length - 1; i++)
             {//如果身份证号已经存在，那么nodeDict一直能拿到相应的Node
                 dictIndex = parse(credinumber[i]);//转换为index
                 if(nodeDict[dictIndex] == null)
-                {//没有这个，说明一定不存在这个身份证号
+                {//没有这个，说明一定不存在这个身份证号,则新建节点
                     isExist = false;
-                    nodeDict[dictIndex] = new Node();
+                    nodeDict[dictIndex] = new object[11];
                 }
-                nodeDict = nodeDict[dictIndex].next;
+                nodeDict = (object[])nodeDict[dictIndex];
             }
             dictIndex = parse(credinumber[credinumber.Length - 1]);
             if (nodeDict[dictIndex] == null)
-            {
+            {//将最后一个赋值为数据库名
                 isExist = false;
-                nodeDict[dictIndex] = new TagNode(currentDatabase);
+                nodeDict[dictIndex] = currentDatabase;
             }
             if(isExist)
             {//冲突
-                conflictDatabase = ((TagNode)(nodeDict[dictIndex])).databaseName;//冲突的话，给conflictDatabase赋值
+                conflictDatabase = (string)nodeDict[dictIndex]; //冲突的话，给conflictDatabase赋值
                 return false;//插入失败
             } else
             {//不冲突
