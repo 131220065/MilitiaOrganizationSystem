c85df0597283d1287cbd19e4d4886b80275cb74c
131220065 <1519261432@qq.com>
2016-07-21 19:23:27 +0800

    王凯军调整了编辑界面的显示

--- 源代码/MilitiaOrganizationSystem/MilitiaOrganizationSystem/Forms/MilitiaEditDialog.Designer.cs
+++ 源代码/MilitiaOrganizationSystem/MilitiaOrganizationSystem/Forms/MilitiaEditDialog.Designer.cs
@@ -36,9 +36,9 @@
             // btn_Cancel
             // 
             this.btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btn_Cancel.Location = new System.Drawing.Point(452, 221);
+            this.btn_Cancel.Location = new System.Drawing.Point(346, 81);
             this.btn_Cancel.Name = "btn_Cancel";
-            this.btn_Cancel.Size = new System.Drawing.Size(75, 187);
+            this.btn_Cancel.Size = new System.Drawing.Size(118, 44);
             this.btn_Cancel.TabIndex = 15;
             this.btn_Cancel.Text = "取消";
             this.btn_Cancel.UseVisualStyleBackColor = true;
@@ -46,9 +46,9 @@
             // btn_OK
             // 
             this.btn_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btn_OK.Location = new System.Drawing.Point(452, 12);
+            this.btn_OK.Location = new System.Drawing.Point(346, 12);
             this.btn_OK.Name = "btn_OK";
-            this.btn_OK.Size = new System.Drawing.Size(75, 171);
+            this.btn_OK.Size = new System.Drawing.Size(118, 44);
             this.btn_OK.TabIndex = 14;
             this.btn_OK.Text = "确定";
             this.btn_OK.UseVisualStyleBackColor = true;
@@ -59,8 +59,8 @@
             this.tlp.AutoScroll = true;
             this.tlp.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tlp.ColumnCount = 2;
-            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.76526F));
-            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.23474F));
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.06785F));
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.93215F));
             this.tlp.Dock = System.Windows.Forms.DockStyle.Left;
             this.tlp.Location = new System.Drawing.Point(0, 0);
             this.tlp.Name = "tlp";
@@ -83,7 +83,7 @@
             this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp.Size = new System.Drawing.Size(446, 416);
+            this.tlp.Size = new System.Drawing.Size(340, 454);
             this.tlp.TabIndex = 16;
             // 
             // MilitiaEditDialog
@@ -92,7 +92,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btn_Cancel;
-            this.ClientSize = new System.Drawing.Size(534, 416);
+            this.ClientSize = new System.Drawing.Size(464, 454);
             this.Controls.Add(this.tlp);
             this.Controls.Add(this.btn_OK);
             this.Controls.Add(this.btn_Cancel);
